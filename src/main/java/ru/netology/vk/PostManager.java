a7fee3ccdb44872be52f70e5a362caabf542d506
Saiferin <54333751+Saiferin@users.noreply.github.com>
2020-12-12 21:17:49 +0300

    Add files via upload

---
+++ src/main/java/ru/netology/vk/PostManager.java
@@ -0,0 +1,17 @@
+package ru.netology.manager;
+
+
+import ru.netology.vk.Post;
+
+public class PostManager {
+    private Post[] search;
+
+    public Post[] search() {
+        // TODO: add logic
+        return null;
+    }
+
+    public void delete() {
+        // TODO: add logic
+    }
+}
